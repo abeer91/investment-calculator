412616b5f2a60e342c94ba2026ba2c5618803647
Abeer Sethi <38733827+abeer91@users.noreply.github.com>
2025-12-14 23:17:37 -0800

    chore: add github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/investment-calculator/',
   test: {
     environment: 'jsdom',
     setupFiles: './vitest.setup.js',
